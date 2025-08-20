5c420158b5f49290063bcb6216391589c5634e59
moosewithwolf <moosewithwolf@gmail.com>
2025-08-20 00:22:33 -0400

    Initial commit

---
+++ src/test/java/me/shinseong/moose/MooseApplicationTests.java
@@ -0,0 +1,13 @@
+package me.shinseong.moose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MooseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
